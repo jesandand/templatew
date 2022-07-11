3c506e32e73b6bb707e217ae69cace273e1e2e98
Kuh <73922679+ramadhankukuh@users.noreply.github.com>
2022-07-11 22:17:52 +0700

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,21 +6,31 @@ const sr = ScrollReveal({
     reset: true
 })
 
+
+/*UTAMA*/
+sr.reveal('.text', {delay: 200})
 sr.reveal('.title', {delay: 300})
 
-/*SCROLL HOME*/
-sr.reveal('.text-1', {delay: 300})
-sr.reveal('.text-2', {delay: 300})
-sr.reveal('.text-3', {delay: 300})
 
-/*SCROLL ABOUT*/
+/*HOME SECTION*/
+sr.reveal('.text-1', {origin:'left', delay: 300})
+sr.reveal('.text-2', {origin:'left', delay: 300})
+sr.reveal('.text-3', {origin:'left', delay: 300})
 
+
+/*ABOUT SECTION*/
 sr.reveal('.about_srollanimation', {delay: 500})
 sr.reveal('p', {origin:'left', delay: 500})
 
-/*SCROLL SKILLS*/
-sr.reveal('.fas', {delay: 500})
 
+/*SERVICE SECTION*/
+sr.reveal('.fa-paint-brush', {delay: 200})
+sr.reveal('.fa-chart-line', {delay: 200})
+sr.reveal('.fa-code', {delay: 200})
+
+
+/*SKILL SECTION*/
+sr.reveal('.fas left', {delay: 500})
 
 
 /*SCROLL CONTACT*/
